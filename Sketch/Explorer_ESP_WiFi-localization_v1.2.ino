0f0c5929dc908744a1b970639d0717a9aed71f5e
Dominik van Veen <Dominikvanveen@gmail.com>
2017-11-23 14:11:49 +0200

    wifi-localization: Change readme to reflect new insights

--- Sketch/Explorer_ESP_WiFi-localization_v1.2.ino
+++ Sketch/Explorer_ESP_WiFi-localization_v1.2.ino
@@ -1,18 +1,12 @@
 #include <TheThingsNetwork.h>
 
-// Used for changing the Bssid hex format to binary. Putting one pair into one byte
 #define HEX_CHAR_TO_NIBBLE(c) ((c >= 'a') ? (c - 'a' + 0x0A) : (c - '0'))
 #define HEX_PAIR_TO_BYTE(h, l) ((HEX_CHAR_TO_NIBBLE(h) << 4) + HEX_CHAR_TO_NIBBLE(l))
 
 // Parameters for the esp stream/buffer
 #define SF_ESP8266_DEFAULT_WAIT 5000
 #define SF_ESP8266_BUFFER_SIZE 128
-
-// The standard speed of the ESP-01 module is 115200. This can be changed with the following command AT+CIOBAUD= *Desired serial speed e.g. 9600*
-//(Note: The default speed can not be used with the explorer board, as its serial port can only handle up to 57600
 #define ESP_SERIAL_SPEED 9600
-
-// Total nummber of access points you want to read/use, and Bssid address size (used for the size of the array)
 #define ACCESS_POINTS 3
 #define SF_WIFI_BSSID_SIZE 6
 
@@ -26,7 +20,7 @@ const char *appKey = "00000000000000000000000000000000";
 // Define the used serials
 #define debugSerial SerialUSB
 #define loraSerial Serial2
-#define espSerial Serial // When using the sodaq explorer connect the ESP TX to pin D0 and RX to D1
+#define espSerial Serial
 
 // Prototyping the functions
 char *readLine(uint32_t waitTime = SF_ESP8266_DEFAULT_WAIT);
@@ -34,137 +28,135 @@ bool waitForOK(uint32_t waitTime = SF_ESP8266_DEFAULT_WAIT);
 void sendBssid();
 
 // Declaring variables
-uint32_t timeout;
-uint8_t bssidByte = 0;
-uint8_t bssidNumber = 0;
 uint8_t payloadLength = ACCESS_POINTS * SF_WIFI_BSSID_SIZE;
-char buffer[SF_ESP8266_BUFFER_SIZE]; // Buffer for the ESP stream
-byte aps[ACCESS_POINTS][SF_WIFI_BSSID_SIZE]; // The array that holds the Bssid's
+char buffer[SF_ESP8266_BUFFER_SIZE];
+byte aps[ACCESS_POINTS][SF_WIFI_BSSID_SIZE];
 
 // TTN constructor
 TheThingsNetwork ttn(loraSerial, debugSerial, freqPlan);
 
 void setup()
 {
-// Wait a maximum of 10s for Serial Monitor
-	while (!debugSerial && millis() < 10000);
+  // Wait a maximum of 10s for Serial Monitor
+  while (!debugSerial && millis() < 10000)
+    ;
 
-	loraSerial.begin(57600);
-	debugSerial.begin(ESP_SERIAL_SPEED);
-	espSerial.begin(ESP_SERIAL_SPEED);
+  loraSerial.begin(57600);
+  debugSerial.begin(ESP_SERIAL_SPEED);
+  espSerial.begin(ESP_SERIAL_SPEED);
 
-	debugSerial.println("-- STATUS");
-	ttn.showStatus();
+  debugSerial.println("-- STATUS");
+  ttn.showStatus();
 
-	debugSerial.println("-- JOIN");
-	ttn.join(appEui, appKey);
+  debugSerial.println("-- JOIN");
+  ttn.join(appEui, appKey);
 }
 
 void loop()
 {
-	bssidNumber = 0;
+  uint8_t bssidNumber = 0;
+  uint32_t timeout;
+  // command to esp, set station mode
+  espSerial.println(F("AT+CWMODE=1"));
+  bool x = waitForOK();
 
-// command to esp, set station mode
-	espSerial.println(F("AT+CWMODE=1"));
-	bool x = waitForOK();
+  if (!x)
+  {
+    debugSerial.println(F("Set mode failed"));
+    goto exit;
+  }
 
-	if (!x)
-	{
-		debugSerial.println(F("Set mode failed"));
-		goto exit;
-	}
+  // Command to esp to get accesspoint information
+  espSerial.println(F("AT+CWLAP"));
 
-// Command to esp to get accesspoint information
-	espSerial.println(F("AT+CWLAP"));
+  char *line;
+  timeout = millis() + 3000;
 
-	char *line;
-	timeout = millis() + 3000;
+  while (millis() < timeout && bssidNumber < ACCESS_POINTS)
+  {
+    line = readLine();
+    debugSerial.println(line);
+    if (strncmp("OK", line, 2) == 0)
+    {
+      debugSerial.println(F("end of access points"));
+      break;
+    }
+    if (strncmp("+CWLAP", line, 2) != 0)
+    {
+      delay(1);
+      debugSerial.println(F("No Bssid"));
+      continue;
+    }
 
-	while (millis() < timeout && bssidNumber < ACCESS_POINTS)
-	{
-		line = readLine();
-		debugSerial.println(line);
-		if (strncmp("OK", line, 2) == 0)
-		{
-			debugSerial.println(F("end of access points"));
-			break;
-		}
-		if (strncmp("+CWLAP", line, 2) != 0)
-		{
-			delay(1);
-			debugSerial.println(F("No Bssid"));
-			continue;
-		}
+    // Break the accesspoint in tokens, to get the bssid out.
+    strtok(line, "\"");
+    strtok(NULL, "\"");
+    strtok(NULL, "\"");
+    char *bssid = strtok(NULL, "\"");
 
-// Break the accesspoint in tokens, to get the bssid out.
-		strtok(line, "\"");
-		strtok(NULL, "\"");
-		strtok(NULL, "\"");
-		char *bssid = strtok(NULL, "\"");
+    // Check if a valid bssid string has been found
+    if (bssid && strlen(bssid) == SF_WIFI_BSSID_SIZE * 3 - 1)
+    {
+      debugSerial.print(F("Bssid: "));
+      debugSerial.println(bssid);
 
-// Check if a valid bssid string has been found
-		if (bssid && strlen(bssid) == SF_WIFI_BSSID_SIZE * 3 - 1)
-		{
-			debugSerial.print(F("Bssid: "));
-			debugSerial.println(bssid);
+      // putting the bssid into an array for easy access
+      char *b = strtok(bssid, ":");
+      for (bssidByte = 0; bssidByte < SF_WIFI_BSSID_SIZE && b; bssidByte++, b = strtok(NULL, ":"))
+      {
+        aps[bssidNumber][bssidByte] = HEX_PAIR_TO_BYTE(b[0], b[1]);
+      }
+      bssidNumber++;
+    }
+  }
 
-// putting the bssid into an array for easy access
-			char *b = strtok(bssid, ":");
-			for (bssidByte = 0; bssidByte < SF_WIFI_BSSID_SIZE && b; bssidByte++, b = strtok(NULL, ":"))
-			{
-				aps[bssidNumber][bssidByte] = HEX_PAIR_TO_BYTE(b[0], b[1]);
-			}
-			bssidNumber++;
-		}
-	}
-
-	sendBssid();
-	exit:
-	debugSerial.println("Restart the loop");
+  sendBssid();
+exit:
+  debugSerial.println("Restart the loop");
 }
 
 bool waitForOK(uint32_t waitTime)
 {
-	uint32_t timeout = millis() + waitTime;
-	while (millis() < timeout)
-	{
-		uint8_t l = espSerial.readBytesUntil('\n', buffer, SF_ESP8266_BUFFER_SIZE);
-		if (l > 0 && strncmp("OK", buffer, 2) == 0)
-		{
-			return true;
-		}
-		delay(1);
-	}
-	return false;
+  uint32_t timeout = millis() + waitTime;
+  while (millis() < timeout)
+  {
+    uint8_t l = espSerial.readBytesUntil('\n', buffer, SF_ESP8266_BUFFER_SIZE);
+    if (l > 0 && strncmp("OK", buffer, 2) == 0)
+    {
+      return true;
+    }
+    delay(1);
+  }
+  return false;
 }
 
 char *readLine(uint32_t waitTime)
 {
-	uint32_t timeout = millis() + waitTime;
-	while (millis() < timeout)
-	{
-		uint8_t l = espSerial.readBytesUntil('\n', buffer, SF_ESP8266_BUFFER_SIZE);
-		if (l > 0)
-		{
-			buffer[l - 1] = '\0';
-			return buffer;
-		}
-		delay(1);
-	}
-	return NULL;
+  uint32_t timeout = millis() + waitTime;
+  while (millis() < timeout)
+  {
+    uint8_t l = espSerial.readBytesUntil('\n', buffer, SF_ESP8266_BUFFER_SIZE);
+    if (l > 0)
+    {
+      buffer[l - 1] = '\0';
+      return buffer;
+    }
+    delay(1);
+  }
+  return NULL;
 }
 
 void sendBssid()
 {
-	byte payload[payloadLength];
-	uint8_t payloadByte = 0;
-	for (bssidNumber = 0; bssidNumber < ACCESS_POINTS; bssidNumber++)
-	{
-		for (bssidByte = 0; bssidByte < SF_WIFI_BSSID_SIZE; bssidByte++, payloadByte++)
-		{
-			payload[payloadByte] = aps[bssidNumber][bssidByte];
-		}
-	}
-	ttn.sendBytes(payload, sizeof(payload));
-	delay(300000);
+  byte payload[payloadLength];
+  uint8_t payloadByte = 0;
+  for (bssidNumber = 0; bssidNumber < ACCESS_POINTS; bssidNumber++)
+  {
+    for (bssidByte = 0; bssidByte < SF_WIFI_BSSID_SIZE; bssidByte++, payloadByte++)
+    {
+      payload[payloadByte] = aps[bssidNumber][bssidByte];
+    }
+  }
+  ttn.sendBytes(payload, sizeof(payload));
+  delay(300000);
 }
